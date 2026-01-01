49bcfdce363b1772ad01b4efec4192788a2d5105
josuekla <josueklayslerjk@gmail.com>
2026-01-01 20:01:38 -0300

    feat: add filtering by category to piadas API route

--- src/routes/piadas.routes.js
+++ src/routes/piadas.routes.js
@@ -2,6 +2,11 @@ import express from "express";
 const router = express.Router();
 import piadas from "../data/piadas.js";
 router.get("/", (req, res) => {
+  const { categoria } = req.query;
+  if (categoria) {
+    const filtradas = piadas.filter((piadas) => piadas.categoria === categoria);
+    return res.json(filtradas);
+  }
   res.json(piadas);
 });
 
