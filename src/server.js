9f1305e5e8864c985d508367c2ef0f885fbfcffa
josuekla <josueklayslerjk@gmail.com>
2026-01-13 17:20:16 -0300

    feat: restructure server setup with caching, blacklist middleware, and authentication route

--- src/server.js
+++ src/server.js
@@ -1,12 +1,38 @@
 import express from "express";
 import indexRoutes from "./routes/index.routes.js";
 import piadasRoutes from "./routes/piadas.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import authenticate from "../middlewares/auth.js";
+import helmet from "helmet";
+import apicache from "apicache";
+
 const app = express();
+const cache = apicache.middleware;
 
-app.use(express.json());
+app.set('trust proxy', 1);
+app.use(express.json());  // Mova para antes das rotas
+app.use(helmet());
 
-app.use("/api/v1/piadas", piadasRoutes);
+// Cache apenas para rotas públicas (não auth)
+app.use('/api/v1/piadas', cache('2 minutes'));
+
+const blacklist = ["1.2.3.4"];
+
+app.use((req, res, next) => {
+  if (blacklist.includes(req.ip)) {
+    return res.status(403).json({ error: "IP bloqueado" });
+  }
+  next();
+});
+
+app.use("/auth", authRoutes);
 app.use("/", indexRoutes);
+app.use("/api/v1/piadas", piadasRoutes);
+
+app.get('/protected', authenticate, (req, res) => {
+    res.json({ message: 'Acesso autorizado', user: req.user });
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
